d6629aacff08ef0d51e9308b71bb09f4dfb8f44d
Carlos De la Rosa <candela2920@gmail.com>
2024-01-03 11:18:00 -0500

    Added logic to prevent errors, in create section

--- create.js
+++ create.js
@@ -1,30 +1,37 @@
-
 let QuizQuestions = new Map();
 let quiz = {
-     name : quizzName,
-     author : undefined,
-     description: descrip,
-     questions : quizQuestions,
-     answerKey: undefined
-}
+  name: quizzName,
+  author: undefined,
+  description: descrip,
+  questions: quizQuestions,
+  answerKey: undefined,
+};
 
-function getNameAndDescription(){
-     quiz.name = document.forms["create"]["quizz-name"].value;     
-     quiz.description = document.forms["create"]["quizz-description"].value;
-}
+const getNameAndDescription = () => {
+  event.preventDefault()
+  quiz.name = document.forms["create"]["quizz-name"].value;
+  quiz.description = document.forms["create"]["quizz-description"].value;
+  if(quiz.name != "" && quiz.description != ""){
+     questionCreator()
+  }
+};
 
-function questionCreator(){
-     let creatorBox = document.getElementById('creator-box');
-     creatorBox.innerHTML = `
+const questionCreator = () => {
+  let creatorBox = document.getElementById("creator-box");
+  creatorBox.innerHTML = `
      <form name = "create-question">
           <div class = "question-container">
           <input type = "text" id = "pregunta" placeholder = "Write the question here" class = "create-input" autocomplete = "off">
           </div>
           <div class = "answer-container">
                <input type = "text" placeholder = "Write the answer here" class = "create-input">
+               <input type = "checkbox">
                <input type = "text" placeholder = "Write the answer here" class = "create-input">
+               <input type = "checkbox">
                <input type = "text" placeholder = "Write the answer here" class = "create-input">
+               <input type = "checkbox">
                <input type = "text" placeholder = "Write the answer here" class = "create-input">
+               <input type  = "checkbox">
           </div>
           <div class = "button-container">
                <button>Previous Question</button>
@@ -33,25 +40,30 @@ function questionCreator(){
           </div>     
           
      </form>`;
-}
-
-const addQuestion = () => {
-     event.preventDefault();
-     input = document.getElementsByClassName('create-input');
-     let question = input[0].value;
-     let answer = [];
-     for(let i = 1; i < input.length; i++){
-          answer.push(input[i].value);
-     }
-     QuizQuestions.set(question,answer);
-     console.log(QuizQuestions);
-     
 };
 
+const addQuestion = () => {
+  event.preventDefault();
+  input = document.getElementsByClassName("create-input");
+  let question = input[0].value;
 
+  if (QuizQuestions.length > 0) {
+    QuizQuestions.forEach((value, key) => {
+      if (key == question) {
+        alert("No se puede agregar preguntas repitidas!");
+      }
+    });
+  }
+
+  let answer = [];
+  for (let i = 1; i < input.length; i++) {
+    answer.push(input[i].value);
+  }
+  QuizQuestions.set(question, answer);
+  console.log(QuizQuestions);
+};
 
 const finish = () => {
-     event.preventDefault();
-     localStorage.setItem(quiz.name, quiz)
-
-}
+  event.preventDefault();
+  localStorage.setItem(quiz.name, quiz);
+};
